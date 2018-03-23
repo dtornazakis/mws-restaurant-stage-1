39267ded8fe2f027c28740a31cc8bc191d673db7
Dimitris Tornazakis <dt@blackfox.gr>
2018-03-23 12:26:42 +0200

    refactor: Responsive re-design on components
    Style the restaurant list items. Style the review list items
    General styling on containers on both desktop and mobile viewports.
    Alter restaurant_info.js file to add classes for styling purposes
    
    TODO: Style working hours table
    TODO: Style filter component

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -117,16 +117,24 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+
+  const header = document.createElement('header');
+
   const name = document.createElement('p');
+  name.className = 'name'
   name.innerHTML = review.name;
-  li.appendChild(name);
+  header.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  date.className = 'date'
+  header.appendChild(date);
 
+  li.appendChild(header);
+  
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'rating'
   li.appendChild(rating);
 
   const comments = document.createElement('p');
