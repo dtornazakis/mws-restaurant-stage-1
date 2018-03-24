8ccc2c3ffaf5caf5f9493b56ab87a9acfc1dd2ea
Dimitris Tornazakis <dt@blackfox.gr>
2018-03-24 12:17:22 +0200

    refactor: Tweak designs
    Re-style table component for restaurant hours to be more prevalent.
    Style Google Maps on restaurant page to be aligned properly. General
    tweaks and styling details around the application

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -75,6 +75,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
  */
 fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
   const hours = document.getElementById('restaurant-hours');
+  
   for (let key in operatingHours) {
     const row = document.createElement('tr');
 
